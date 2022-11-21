2fb9b0e8370974b028b3c3ced5bd7ddfab6672d9
Anna Richter <awiedenroth@uos.de>
2022-11-21 17:13:59 -0500

    Update run_transformer.sh

--- examples/pytorch/text-classification/run_transformer.sh
+++ examples/pytorch/text-classification/run_transformer.sh
@@ -17,8 +17,9 @@ module load python/3.9
 
 source ~/transformerenv/bin/activate
 
-DATA_TRAIN=$GIT_DIR/Annas_data/train.csv
-DATA_TEST=$GIT_DIR/Annas_data/test.csv
+DATA_TRAIN=$GIT_DIR/Annas_data/othertrain.csv
+DATA_EVAL=$GIT_DIR/Annas_data/othertest.csv
+DATA_TEST=$GIT_DIR/Annas_data/oureval.csv
 OUT_PATH=$GIT_DIR/Annas_output
 
-python run_glue.py --model_name_or_path bert-base-cased --do_train --do_predict --max_seq_length 128 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 --output_dir $OUT_PATH --train_file $DATA_TRAIN --test_file $DATA_TEST
+python3.9 run_glue.py --model_name_or_path bert-base-cased --do_train --max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 --output_dir $OUT_PATH --train_file $DATA_TRAIN --validation_file $DATA_EVAL --do_eval --overwrite_output_dir --do_predict --test_file $DATA_TEST
